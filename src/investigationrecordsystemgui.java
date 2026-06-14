e2f010e3c77e418939170aa27be2aedcda4d8132
HP <abidhassanw2@gmail.com>
2026-06-14 08:01:58 -0700

    initial commit

--- src/investigationrecordsystemgui.java
+++ src/investigationrecordsystemgui.java
@@ -25,3 +25,32 @@ class crimerecord {
                 ", Status: " + status;
     }
 }
+public class investigationrecordsystemgui extends JFrame {
+
+    JTextField idField, typeField, suspectField, statusField;
+    JTextArea outputArea;
+
+    HashMap<Integer, CrimeRecord> crimeMap = new HashMap<>();
+
+    public investigationrecordsystemgui() {
+
+        setTitle("Crime Investigation Record System");
+        setSize(700, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JPanel panel = new JPanel(new GridLayout(5, 2));
+
+        panel.add(new JLabel("Crime ID:"));
+        idField = new JTextField();
+        panel.add(idField);
+
+        panel.add(new JLabel("Crime Type:"));
+        typeField = new JTextField();
+        panel.add(typeField);
+
+        panel.add(new JLabel("Suspect Name:"));
+        suspectField = new JTextField();
+        panel.add(suspectField);
+
+    }}
